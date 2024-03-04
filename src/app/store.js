15ffbe9e6c25dcb8e9e39b1e2c77d59c363e6be5
berkant unal <berkantunaldev@gmail.com>
2024-03-05 00:40:53 +0300

    fix netlify build

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import noteReducer from '../features/note/noteSlice'
-import listReducer from '../features/list/listslice'
+import listReducer from '../features/list/listSlice'
 import modalReducer from '../features/modal/modalSlice'
 import preferencesReducer from '../features/preferences/preferencesSlice'
 
